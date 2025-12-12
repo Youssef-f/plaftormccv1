43c170e142bdc08a8f52402a2a4c5e4b552dfb1b
youssef <fakir.youssef0996@gmail.com>
2025-12-12 12:39:37 +0100

    fixing more bugs

--- src/main/java/org/platformv1/platformccv1/config/CorsConfig.java
+++ src/main/java/org/platformv1/platformccv1/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","https://plaftormccv1-frontend-4j78908la-sefyus-projects.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization","Content-Type")
